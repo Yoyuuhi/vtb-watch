feb8341a3182a5e72d3c5956e3485c4d84f14903
Kishou <xijie931025@gmail.com>
2020-04-05 03:00:55 +0900

    mylists-indexビュー草案

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'mylists/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "mylists#index"
 end
