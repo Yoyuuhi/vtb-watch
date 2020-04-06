e5a44dceb8e7ba5e0f8fbb09802ebdf9afd6fcb7
Kishou <xijie931025@gmail.com>
2020-04-07 01:25:26 +0900

    ビュー更新20200406

--- app/controllers/mylists_controller.rb
+++ app/controllers/mylists_controller.rb
@@ -3,8 +3,33 @@ class MylistsController < ApplicationController
   end
 
   def new
+    @mylist = Mylist.new
   end
 
   def create
+    @mylist = Mylist.new(mylist_params)
+    if @mylist.save
+      redirect_to root_path, notice: 'マイリストを作成しました'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @mylist = Mylist.find(params[:id])
+  end
+
+  def update
+    @mylist = Mylist.find(params[:id])
+    if @mylist.update(mylist_params)
+      redirect_to root_path, notice: 'マイリストを更新しました'
+    else
+      render :edit
+    end
+  end
+
+  private
+  def mylist_params
+    params.require(:mylist).permit(:name, vtuber_ids: []).merge(user_id: current_user.id)
   end
 end
--- app/models/mylist.rb
+++ app/models/mylist.rb
@@ -1,5 +1,5 @@
 class Mylist < ApplicationRecord
   has_many :vtubers, through: :mylist_vtubers
-  has_many :mylist_users
+  has_many :mylist_vtubers
   belongs_to :user
 end
