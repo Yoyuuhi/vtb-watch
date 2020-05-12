77da57ef6745740f64249f19b0dcee32038291f9
Kishou <xijie931025@gmail.com>
2020-05-13 00:03:24 +0900

    ActionMailerの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_host
 
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def set_host
+    Rails.application.routes.default_url_options[:host] = request.host_with_port
   end
 end
