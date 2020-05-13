7bc8842a9d5db160d5882122f0c6729615413a32
Kishou <xijie931025@gmail.com>
2020-05-13 10:40:29 +0900

    default_url_options変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :set_host
+  # before_action :set_host
 
   protected
   def configure_permitted_parameters
