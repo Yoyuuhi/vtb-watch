8e2bd47d9b01f62e1ea933ae1da57be4c6870cdc
Kishou <xijie931025@gmail.com>
2020-05-13 11:18:42 +0900

    production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   domain:               'gmail.com',
   user_name:            ENV["GMAIL_ACCOUNT"],
   password:             ENV["GMAIL_PASSWORD"],
-  authentication:       'plain',
+  authentication:       'login',
   enable_starttls_auto:  true
   }
 end
